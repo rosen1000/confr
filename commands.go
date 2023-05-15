aacf13610a3c51ee32fb7476f28e64cc3fee037a
rosen1000 <rosen.ruseff@gmail.com>
2023-05-15 06:05:35 +0300

    Extracted subcommands to commands.go

---
+++ commands.go
@@ -0,0 +1,168 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"syscall"
+
+	ansicodes "github.com/azer/go-ansi-codes"
+	"github.com/spf13/cobra"
+)
+
+var ignoreTime bool
+
+func InitCommands(root *cobra.Command) {
+	saveCmd.Flags().BoolVar(&ignoreTime, "ignore-time", false, "Bypasses check if a config has changed")
+
+	root.AddCommand(lsCmd)
+	root.AddCommand(saveCmd)
+	root.AddCommand(rmCmd)
+	root.AddCommand(initCmd)
+	root.AddCommand(restoreCmd)
+}
+
+var lsCmd = &cobra.Command{
+	Use:     "ls",
+	Aliases: []string{"list"},
+	Short:   "List available configs",
+	Run: func(cmd *cobra.Command, args []string) {
+		conf := ReadFile()
+		text := []string{}
+		for i := 0; i < len(conf.Files); i++ {
+			file := conf.Files[i]
+			text = append(text, fmt.Sprintf("%v%v%v:\n  Size: %v bytes\n  Path: %v\n  Modified: %v", ansicodes.Blue, file.DisplayName, ansicodes.Reset, len(file.Content), file.Path, file.Modified))
+		}
+		fmt.Println(strings.Join(text, "\n"))
+	},
+}
+
+var saveCmd = &cobra.Command{
+	Use:   "save [name] [path]",
+	Short: "Save file to configs",
+	Run: func(cmd *cobra.Command, args []string) {
+		conf := ReadFile()
+
+		replace := false
+		replaceIndex := 0
+		displayName, filePath := args[0], args[1]
+		stats, err := os.Stat(filePath)
+		CatchErr(err, "Error while checking file:")
+		for i, file := range conf.Files {
+			if !ignoreTime && file.Modified.Equal(stats.ModTime()) {
+				fmt.Println("File not changed. Ignoring")
+				return
+			}
+
+			absPath, err := filepath.Abs(filePath)
+			CatchErr(err, "File not found:")
+			if file.DisplayName == displayName || file.Path == absPath {
+				fmt.Printf("Found the following:\n  Name: %v\n  Size: %v bytes\n  Path: %v\n  Modified: %v\n", file.DisplayName, len(file.Content), file.Path, file.Modified)
+				fmt.Print("Overwrite? (y/n) ")
+				option := ReadLine()
+				if option == "y" || option == "yes" {
+					replace = true
+					replaceIndex = i
+					break
+				}
+				return
+			}
+		}
+
+		var file FileJSON
+		file.DisplayName = args[0]
+		path, err := filepath.Abs(args[1])
+		CatchErr(err)
+		file.Path = path
+
+		if stats.IsDir() {
+			panic("Directories are not implemented")
+		}
+
+		User, err := (user.LookupId(strconv.Itoa(int(stats.Sys().(*syscall.Stat_t).Uid))))
+		CatchErr(err, "Couldn't get user owenership")
+		Group, err := (user.LookupGroupId(strconv.Itoa(int(stats.Sys().(*syscall.Stat_t).Gid))))
+		CatchErr(err, "Couldn't get group ownership")
+
+		file.Permisions = fmt.Sprintf("%v:%v %v", User.Username, Group.Name, stats.Mode())
+		file.Modified = stats.ModTime()
+
+		bytes, err := os.ReadFile(file.Path)
+		CatchErr(err)
+		file.Content = string(bytes)
+
+		if replace {
+			fmt.Println()
+			conf.Files = append(conf.Files[:replaceIndex], conf.Files[:replaceIndex+1]...)
+		} else {
+			conf.Files = append(conf.Files, file)
+		}
+
+		WriteFile(conf)
+		fmt.Println("Saved!")
+	},
+	Args: cobra.ExactArgs(2),
+}
+
+var rmCmd = &cobra.Command{
+	Use:   "rm [config-name]",
+	Short: "Remove config",
+	Run: func(cmd *cobra.Command, args []string) {
+		conf := ReadFile()
+		for i, file := range conf.Files {
+			if strings.Contains(file.DisplayName, args[0]) {
+				fmt.Println("Found:", file.DisplayName)
+				conf.Files = append(conf.Files[:i], conf.Files[i+1:]...)
+				WriteFile(conf)
+				return
+			}
+		}
+		fmt.Println("Nothing found")
+	},
+	Args: cobra.ExactArgs(1),
+}
+
+var initCmd = &cobra.Command{
+	Use:   "init",
+	Short: "Explicidly init config file storage",
+	Long:  "Explicidly initialize config file storage and provide all information about it like location and such",
+	Run: func(cmd *cobra.Command, args []string) {
+		conf := ConfJSON{}
+		User, err := user.Current()
+		CatchErr(err)
+		conf.User = User.Username
+		conf.HomePath = os.Getenv("HOME")
+		WriteFile(conf)
+	},
+}
+
+var restoreCmd = &cobra.Command{
+	Use:   "restore [search]",
+	Short: "Restore stored configs",
+	Run: func(cmd *cobra.Command, args []string) {
+		var options []FileJSON
+		search := args[0]
+		conf := ReadFile()
+		for _, file := range conf.Files {
+			if strings.Contains(file.Path, search) || strings.Contains(file.DisplayName, search) {
+				options = append(options, file)
+			}
+		}
+
+		for i, option := range options {
+			fmt.Printf("%d: %s %s\n", i+1, option.DisplayName, option.Path)
+		}
+
+		option := StringRange(ReadLine())
+		for _, i := range option {
+			file := options[i-1]
+			err := file.write()
+			CatchErr(err)
+			fmt.Println(options[i-1].Path)
+		}
+	},
+	Args: cobra.ExactArgs(1),
+}
--- confr.go
+++ confr.go
@@ -6,177 +6,23 @@ import (
 	"fmt"
 	"io/fs"
 	"os"
-	"os/user"
-	"path/filepath"
 	"regexp"
 	"strconv"
 	"strings"
-	"syscall"
 	"time"
 
-	"github.com/azer/go-ansi-codes"
 	"github.com/spf13/cobra"
 )
 
 const CONF_PATH = "./conf.json"
 
 func main() {
-
 	rootCmd := &cobra.Command{
 		Use:   "confr",
 		Short: "Configuration backup tool",
 	}
 
-	lsCmd := &cobra.Command{
-		Use:     "ls",
-		Aliases: []string{"list"},
-		Short:   "List available configs",
-		Run: func(cmd *cobra.Command, args []string) {
-			conf := ReadFile()
-			text := []string{}
-			for i := 0; i < len(conf.Files); i++ {
-				file := conf.Files[i]
-				text = append(text, fmt.Sprintf("%v%v%v:\n  Size: %v bytes\n  Path: %v\n  Modified: %v", ansicodes.Blue, file.DisplayName, ansicodes.Reset, len(file.Content), file.Path, file.Modified))
-			}
-			fmt.Println(strings.Join(text, "\n"))
-		},
-	}
-
-	var ignoreTime bool
-	saveCmd := &cobra.Command{
-		Use:   "save [name] [path]",
-		Short: "Save file to configs",
-		Run: func(cmd *cobra.Command, args []string) {
-			conf := ReadFile()
-
-			replace := false
-			replaceIndex := 0
-			displayName, filePath := args[0], args[1]
-			stats, err := os.Stat(filePath)
-			CatchErr(err, "Error while checking file:")
-			for i, file := range conf.Files {
-				if !ignoreTime && file.Modified.Equal(stats.ModTime()) {
-					fmt.Println("File not changed. Ignoring")
-					return
-				}
-
-				absPath, err := filepath.Abs(filePath)
-				CatchErr(err, "File not found:")
-				if file.DisplayName == displayName || file.Path == absPath {
-					fmt.Printf("Found the following:\n  Name: %v\n  Size: %v bytes\n  Path: %v\n  Modified: %v\n", file.DisplayName, len(file.Content), file.Path, file.Modified)
-					fmt.Print("Overwrite? (y/n) ")
-					option := ReadLine()
-					if option == "y" || option == "yes" {
-						replace = true
-						replaceIndex = i
-						break
-					}
-					return
-				}
-			}
-
-			var file FileJSON
-			file.DisplayName = args[0]
-			path, err := filepath.Abs(args[1])
-			CatchErr(err)
-			file.Path = path
-
-			if stats.IsDir() {
-				panic("Directories are not implemented")
-			}
-
-			User, err := (user.LookupId(strconv.Itoa(int(stats.Sys().(*syscall.Stat_t).Uid))))
-			CatchErr(err, "Couldn't get user owenership")
-			Group, err := (user.LookupGroupId(strconv.Itoa(int(stats.Sys().(*syscall.Stat_t).Gid))))
-			CatchErr(err, "Couldn't get group ownership")
-
-			file.Permisions = fmt.Sprintf("%v:%v %v", User.Username, Group.Name, stats.Mode())
-			file.Modified = stats.ModTime()
-
-			bytes, err := os.ReadFile(file.Path)
-			CatchErr(err)
-			file.Content = string(bytes)
-
-			if replace {
-				fmt.Println()
-				conf.Files = append(conf.Files[:replaceIndex], conf.Files[:replaceIndex+1]...)
-			} else {
-				conf.Files = append(conf.Files, file)
-			}
-
-			WriteFile(conf)
-			fmt.Println("Saved!")
-		},
-		Args: cobra.ExactArgs(2),
-	}
-
-	saveCmd.Flags().BoolVar(&ignoreTime, "ignore-time", false, "Bypasses check if a config has changed")
-
-	rmCmd := &cobra.Command{
-		Use:   "rm [config-name]",
-		Short: "Remove config",
-		Run: func(cmd *cobra.Command, args []string) {
-			conf := ReadFile()
-			for i, file := range conf.Files {
-				if strings.Contains(file.DisplayName, args[0]) {
-					fmt.Println("Found:", file.DisplayName)
-					conf.Files = append(conf.Files[:i], conf.Files[i+1:]...)
-					WriteFile(conf)
-					return
-				}
-			}
-			fmt.Println("Nothing found")
-		},
-		Args: cobra.ExactArgs(1),
-	}
-
-	initCmd := &cobra.Command{
-		Use:   "init",
-		Short: "Explicidly init config file storage",
-		Long:  "Explicidly initialize config file storage and provide all information about it like location and such",
-		Run: func(cmd *cobra.Command, args []string) {
-			conf := ConfJSON{}
-			User, err := user.Current()
-			CatchErr(err)
-			conf.User = User.Username
-			conf.HomePath = os.Getenv("HOME")
-			WriteFile(conf)
-		},
-	}
-
-	restoreCmd := &cobra.Command{
-		Use:   "restore [search]",
-		Short: "Restore stored configs",
-		Run: func(cmd *cobra.Command, args []string) {
-			var options []FileJSON
-			search := args[0]
-			conf := ReadFile()
-			for _, file := range conf.Files {
-				if strings.Contains(file.Path, search) || strings.Contains(file.DisplayName, search) {
-					options = append(options, file)
-				}
-			}
-
-			for i, option := range options {
-				fmt.Printf("%d: %s %s\n", i+1, option.DisplayName, option.Path)
-			}
-
-			option := StringRange(ReadLine())
-			for _, i := range option {
-				file := options[i-1]
-				err := os.WriteFile(file.Path, []byte(file.Content), fs.FileMode(ParsePermissions(file).Perms))
-				CatchErr(err)
-				fmt.Println(options[i-1].Path)
-			}
-		},
-		Args: cobra.ExactArgs(1),
-	}
-
-	rootCmd.AddCommand(lsCmd)
-	rootCmd.AddCommand(saveCmd)
-	rootCmd.AddCommand(rmCmd)
-	rootCmd.AddCommand(initCmd)
-	rootCmd.AddCommand(restoreCmd)
+	InitCommands(rootCmd)
 
 	rootCmd.Execute()
 }
@@ -215,7 +61,7 @@ func ReadLine() string {
 	return scanner.Text()
 }
 
-func ParsePermissions(file FileJSON) Permission {
+func (file FileJSON) ParsePermissions() Permission {
 	var output Permission
 	segments := strings.Split(file.Permisions, " ")
 	userGroup := strings.Split(segments[0], ":")
@@ -242,12 +88,12 @@ func ParsePermissions(file FileJSON) Permission {
 }
 
 func StringRange(ranges string) []int {
-	regex := regexp.MustCompile(`^!?\d+([,-]!?\d+)*$`)
+	regex := regexp.MustCompile(`^!?\d+([, -]!?\d+)*$`)
 	dash := regexp.MustCompile(`^(\d+)-(\d+)$`)
 	if regex.Match([]byte(ranges)) {
 		// compute ranges
 		compiledRange := []int{}
-		segments := strings.Split(ranges, ",")
+		segments := strings.Split(ranges, "[, ]")
 		var negativeSegments []int
 		for _, seg := range segments {
 			if seg[0] == '!' {
@@ -297,6 +143,7 @@ func RemoveDuplicates(nums []int) []int {
 	return result
 }
 
+// If err is not nil, the program will exit with exit code 1
 func CatchErr(err error, msg ...string) {
 	if err != nil {
 		if len(msg) > 0 {
@@ -309,7 +156,7 @@ func CatchErr(err error, msg ...string) {
 }
 
 type ConfJSON struct {
-	User     string
+	User     string `json:"user"`
 	HomePath string
 	Files    []FileJSON
 }
@@ -320,6 +167,10 @@ type FileJSON struct {
 	Path        string
 	Permisions  string
 	Modified    time.Time
+}
+
+func (file FileJSON) write() error {
+	return os.WriteFile(file.Path, []byte(file.Content), fs.FileMode(file.ParsePermissions().Perms))
 }
 
 type Permission struct {
